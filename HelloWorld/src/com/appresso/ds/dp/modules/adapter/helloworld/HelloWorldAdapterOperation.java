15d365c951557e1cac436bef83b0a48f2a6d4741
dataspider-fan <119771635+dataspider-fan@users.noreply.github.com>
2022-12-29 15:33:42 +0900

    Update HelloWorldAdapterOperation.java

--- HelloWorld/src/com/appresso/ds/dp/modules/adapter/helloworld/HelloWorldAdapterOperation.java
+++ HelloWorld/src/com/appresso/ds/dp/modules/adapter/helloworld/HelloWorldAdapterOperation.java
@@ -86,7 +86,7 @@ public class HelloWorldAdapterOperation implements Operation {
 		String ownerAddr_local = keyPair_local.toBase58CheckAddress();
 
 		// 上記、MyTokenのデプロイ後のスマートコントラクトアドレスを指定する
-		String cntrAddr_local = "TEQw51ck1JUja28JQ4Yg8fZsYMM1eXAmLe";
+		String cntrAddr_local = "TTq6PiZee7XHiMUdcY5s4UXSrdXrHnLVbw";
 
 		// getBalance関数の呼び出し
 		Function balanceOf_local = new Function("balanceOf",
@@ -97,6 +97,73 @@ public class HelloWorldAdapterOperation implements Operation {
 		String result_local = Numeric.toHexString(txnExt_local.getConstantResult(0).toByteArray());
 		log.info("balanceOf : " + FunctionReturnDecoder.decode(result_local, balanceOf_local.getOutputParameters()).get(0).getValue());
 
+		
+		/*
+		 * スマートコントラクトの関数呼び出し（更新系）
+		 */
+		// Function transferに渡すパラメータ
+		long amount = 1; 		// 送金額
+		int powerValue = 2; 		// TODO Calculation methods and estimation logic need to be implemented separately.
+		long limitForFee = 5000000; 	// TODO Calculation methods and estimation logic need to be implemented separately.
+		String TransactionMessage = "transferCallForLocal";
+
+		// MyTokenコントラクトアドレス
+		String cntrAddrModify = "TTq6PiZee7XHiMUdcY5s4UXSrdXrHnLVbw";
+
+		// MyTokenコントラクトデプロイアカウントの秘密鍵
+		String privateModifyKey = "e26d5a9e94aea8e35b6b2668c298d01fe09c2490c47d242eb4cfd036188f0b51";
+
+		// localのgRPCサービスに接続
+		ApiWrapper wrapperModify =  new ApiWrapper("127.0.0.1:50051", "127.0.0.1:50052", privateModifyKey);
+
+		// 送金元アドレス　TPPDEF8x6Pe3R16dJJi7Ui8fyjg1k1yUpo 
+		KeyPair keyPairModify = new KeyPair(privateModifyKey);
+		String ownerAddrModify = keyPairModify.toBase58CheckAddress();
+		// 送金先アドレス
+		String destAddr = "TJGD2HHUz2poYebAaCLNxNQE1ZCEJ2nfuu";
+
+		// transferスマートコントラクト呼び出し
+		Function transfer = new Function("transfer",
+						Arrays.asList(new Address(destAddr),
+						new Uint256(BigInteger.valueOf(amount).multiply(BigInteger.valueOf(10).pow(powerValue)))),
+						Arrays.asList(new TypeReference<Bool>() {}));
+
+		TransactionBuilder Trxbuilder = wrapperModify.triggerCall(ownerAddrModify, cntrAddrModify, transfer);
+		Trxbuilder.setFeeLimit(limitForFee);
+		Trxbuilder.setMemo(TransactionMessage);
+
+
+		/*
+		 * 送金先アドレスの残高（送金送金前）
+		*/
+		// getBalance関数の呼び出し
+		balanceOf_local = new Function("balanceOf",
+					Arrays.asList(new Address(destAddr)), Arrays.asList(new TypeReference<Uint256>() {}));
+
+		// getBalanceの取得結果を実行ログに表示
+		txnExt_local = wrapper_local.constantCall(destAddr, cntrAddrModify, balanceOf_local);
+		result_local = Numeric.toHexString(txnExt_local.getConstantResult(0).toByteArray());
+		log.info("送信先 送金前 残高 : " + FunctionReturnDecoder.decode(result_local, balanceOf_local.getOutputParameters()).get(0).getValue());
+
+		 /*
+		 * 送金実行
+		 */
+		log.info("送金実行");
+		Transaction toSendTxn = wrapperModify.signTransaction(Trxbuilder.build());
+		String toSendResult = wrapperModify.broadcastTransaction(toSendTxn);
+		log.info("transactionHash : " + toSendResult);
+
+		/*
+		 * 送金先アドレスの残高（送金後）
+		 */
+		// getBalance関数の呼び出し
+		balanceOf_local = new Function("balanceOf",
+		Arrays.asList(new Address(destAddr)), Arrays.asList(new TypeReference<Uint256>() {}));
+
+		// getBalanceの取得結果を実行ログに表示
+		txnExt_local = wrapper_local.constantCall(destAddr, cntrAddrModify, balanceOf_local);
+		result_local = Numeric.toHexString(txnExt_local.getConstantResult(0).toByteArray());
+		log.info("送信先 送金後 残高 : " + FunctionReturnDecoder.decode(result_local, balanceOf_local.getOutputParameters()).get(0).getValue());
 		
 		return new HashMap();
 	}
